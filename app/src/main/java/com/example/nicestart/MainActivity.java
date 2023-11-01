54345d22797fef4c807be1bdf6bbd54a35ceafd9
Sindy <sindy.nunez@educa.madrid.org>
2023-11-01 20:53:29 +0100

    App

---
+++ app/src/main/java/com/example/nicestart/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.nicestart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+import androidx.core.content.ContextCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Drawable drawable;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView=findViewById(R.id.girl);
+        drawable = ContextCompat.getDrawable(this, R.drawable.girl);
+        imageView.setImageDrawable(drawable);
+    }
+}
